93e79f8049e71c1ae110396f4cd942bb7dbbcdd8
pdevashish <devdewance@gmail.com>
2022-04-09 03:41:27 +0530

    new Release 8

--- src/main/java/com/pdp/reactive/malabar/controller/UserController.java
+++ src/main/java/com/pdp/reactive/malabar/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     @GetMapping("/")
     public Mono<String> Index(){
         log.info("UserController index ....");
-        return Mono.just("App is working -8");
+        return Mono.just("App is working -9");
     }
 
     @GetMapping("/users")
